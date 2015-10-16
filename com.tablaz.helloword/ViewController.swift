5ceac811b563f6cd5c56daba8bc0291c3e57a93f
Ricardo Restrepo M <rrestrepo@seeketing.com>
2015-10-16 12:39:19 +0000

    Initial Commit

---
+++ com.tablaz.helloword/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  com.tablaz.helloword
+//
+//  Created by Ricardo on 16/10/2015.
+//  Copyright © 2015 Ricardo Restrepo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ com.tablaz.hellowordTests/com_tablaz_hellowordTests.swift
@@ -0,0 +1,36 @@
+//
+//  com_tablaz_hellowordTests.swift
+//  com.tablaz.hellowordTests
+//
+//  Created by Ricardo on 16/10/2015.
+//  Copyright © 2015 Ricardo Restrepo. All rights reserved.
+//
+
+import XCTest
+@testable import com_tablaz_helloword
+
+class com_tablaz_hellowordTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
